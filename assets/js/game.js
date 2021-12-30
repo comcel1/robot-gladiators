97a4db3e188028d3d46f24483f0a3f6dc8aef69a
PaulNelsonOld <90969624+comcel1@users.noreply.github.com>
2021-12-30 09:10:45 -0600

    updated js to check robot and player health after attack

--- assets/js/game.js
+++ assets/js/game.js
@@ -27,6 +27,28 @@ console.log(
   console.log(
     enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
   );
+  console.log(
+    playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+  );
+
+  // check enemy's health
+if (enemyHealth <= 0) {
+    window.alert(enemyName + " has died!");
+  } 
+  else {
+    window.alert(enemyName + " still has " + enemyHealth + " health left.");
+  }
 };
+
+// check robot's health
+console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
+
+// check player's health
+if (playerHealth <= 0) {
+  window.alert(playerName + " has died!");
+} 
+else {
+  window.alert(playerName + " still has " + playerHealth + " health left.");
+}
 
 fight();
