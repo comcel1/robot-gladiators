fd9d13a43f1d7ca21097ed53ab58d1477710abee
PaulNelsonOld <90969624+comcel1@users.noreply.github.com>
2021-12-30 21:42:55 -0600

    added console log to variables at top of js

--- assets/js/game.js
+++ assets/js/game.js
@@ -7,7 +7,13 @@ var playerMoney = 10;
 // You can also log multiple values at once like this
 console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
+console.log(enemyNames);
+for(var i = 0; i < enemyNames.length; i++) {
+  console.log(enemyNames[i]);
+  console.log(i);
+  console.log(enemyNames[i] + " is at " + i + " index");
+}
 var enemyHealth = 50;
 var enemyAttack = 12;
 
@@ -68,4 +74,4 @@ var fight = function() {
 };
 
 // run fight function to start game
-fight();
+// fight();
