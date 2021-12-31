eed597b718d1be9f8c37beb55e5e85692d089aaa
PaulNelsonOld <90969624+comcel1@users.noreply.github.com>
2021-12-30 18:20:55 -0600

    pushed initial content of feature to github

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,19 @@
-window.alert("This is an alert! JavaScript is running!");
+// this creates a function named "fight"
+
+var playerName = "Tony the Robot";
+
+// Tony the Robot is ready for battle!
+console.log("Tony the Robot" + " is ready for battle!");
+
+// "Tony the Robot is ready for battle!"
+console.log(playerName + " is ready for battle!");
+
+// "Your robot, Tony the Robot, has won!
+console.log("Your robot, " + playerName + ", has won!");
+
+function fight() {
+  window.alert("The fight has begun!"); // declared the function
+}
+
+// this starts the fight
+//fight();
