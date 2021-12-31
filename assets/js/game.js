15d640b6f4e85f9521c4a336438a09981d973a69
PaulNelsonOld <90969624+comcel1@users.noreply.github.com>
2021-12-30 21:55:45 -0600

    updated var fight function

--- assets/js/game.js
+++ assets/js/game.js
@@ -18,7 +18,8 @@ var enemyHealth = 50;
 var enemyAttack = 12;
 
 // fight function
-var fight = function() {
+var fight = function(enemyName) {
+
   // Alert players that they are starting the round
   window.alert("Welcome to Robot Gladiators!");
 
