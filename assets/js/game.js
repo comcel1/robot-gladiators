10f616f1df6ac04de87f2a1cd41465f8588346a1
PaulNelsonOld <90969624+comcel1@users.noreply.github.com>
2022-01-01 13:23:43 -0600

    fixed blank name bug

--- assets/js/game.js
+++ assets/js/game.js
@@ -169,9 +169,25 @@ var shop = function() {
 
 /* GAME INFORMATION / VARIABLES */
 
+// function to set name
+var getPlayerName = function() {
+  var name = "";
+
+  while (name === "" || name === null) {
+    name = prompt("What is your robot's name?");
+  }
+  
+// ***************************************
+// ADD LOOP HERE WITH PROMPT AND CONDITION
+// ***************************************
+
+  console.log("Your robot's name is " + name);
+  return name;
+};
+
 // player information
 var playerInfo = {
-  name: window.prompt("What is your robot's name?"),
+  name: getPlayerName(),
   health: 100,
   attack: 10,
   money: 10,
